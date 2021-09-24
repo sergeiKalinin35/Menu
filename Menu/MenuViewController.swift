f81460284b1debb3e9829c6030e2ddba22351c21
Sergey <piolersa85@mail.ru>
2021-09-24 16:55:10 +0200

    New Menu

---
+++ Menu/MenuViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MenuViewController.swift
+//  Menu
+//
+//  Created by Sergey on 24.09.2021.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+    
+
+}
